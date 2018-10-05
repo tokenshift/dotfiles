499774b76513fec40a13eebf809054c666d15d7f
Nathan <nathan.clark@tokenshift.com>
2018-10-05 10:12:23 -0400

    No longer removing .git folders.

--- apply.sh
+++ apply.sh
@@ -3,12 +3,4 @@
 git submodule init
 git submodule update --recursive
 
-temp_dir=$(mktemp -d)
-echo "Working folder: $temp_dir"
-
-cp -r ./home/ "$temp_dir"
-find "$temp_dir" -name .git -type f -delete
-
-rsync -avzh "$temp_dir/" "$HOME/"
-
-rm -Rf "$temp_dir"
+rsync -avzh "./home/" "$HOME/"
